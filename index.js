f9331a65234ac95a8768fb4106efca14c38608c2
Jaime Pillora <jpillora@gmail.com>
2013-05-24 23:03:52 +1000

    fixed retriever

--- index.js
+++ index.js
@@ -6,39 +6,54 @@ var MAX_SIZE = 500*1024;
 
 var app = express();
 
+app.use(express.logger("dev"));
 app.use(app.router);
 
 //file retriever via jsonp proxy
 app.get('/retrieve', function(req, res) {
   var callback = req.query.callback || 'callback';
 
-  var url = req.query.url;
-  if(!url)
-    return res.send(400,'No URL\n');
-  var r = null;
+  res.write(callback+'({"body": "');
+
+  var r = null, error = null, url = req.query.url;
+
+  if(!url) error = 'No URL';
+
   try {
     r = request(url);
-  } catch(e) {
-    return res.send(400,'Invalid URL\n');
+  } catch(err) {
+    error = err.toString();
   }
 
   var total = 0;
-  res.write(callback+'("');
-  r.on('data', function(chunk) {
-    total += chunk.length;
-    if(total > MAX_SIZE) {
-      res.end('>>>>> SIZE EXCEEDED!!!');
-      r.abort();
-      return;
-    }
-    res.write(chunk.toString()
-      .replace(/"/g, '\\"')
-      .replace(/\n/g, '\\n')
-    );
-  });
-  r.on('end', function() {
-    res.end('");\n');
-  });
+
+  var kill = function(err) {
+    error = err;
+    end();
+    r.abort();
+  };
+
+  var add = function(obj) {
+
+    total += obj.length;
+
+    if(total > MAX_SIZE)
+      return kill('max size exceeded ('+MAX_SIZE/1024+'Kb)');
+
+    var json = JSON.stringify(obj.toString());
+
+    res.write(json.substr(1,json.length-2));
+  };
+
+  var end = function() {
+    res.end('", "error":'+ (error ? JSON.stringify(error) : 'null') + '});\n');
+  };
+
+  if(error) return end();
+
+  r.on('error', kill);
+  r.on('data', add);
+  r.on('end', end);
 });
 
 //file downloader via post replay
